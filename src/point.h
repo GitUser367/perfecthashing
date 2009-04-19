fee603d4160e78377cb5e8737da603987161b12d
remi.paucher <remi.paucher@student.ecp.fr>
2009-04-19 19:37:55 +0000

    - Implémentation en C++ de l'algorithme de perfect hashing de base - Pour le moment que des images en niveaux de gris (todo: 3D & RGB) - Comme dans §4.1, distinction fast & compact (recherche dichotomique)

---
+++ src/point.h
@@ -0,0 +1,26 @@
+#pragma once
+
+//Classe point (avec éventuellement une couleur associée)
+class Point
+{
+public:
+	Point(int u, int v, unsigned char col=0)
+	{
+		this->x = u;
+		this->y = v;
+		this->color = col;
+	}
+
+	Point (const Point &other)
+	{
+		this->x = other.x;
+		this->y = other.y;
+		this->color = other.color;
+	}
+
+	Point() {};
+
+	int x;
+	int y;
+	unsigned char color;
+};
